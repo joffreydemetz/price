f775c43577795f66d270791d75401d0138111bf8
Joffrey Demetz <joffrey.demetz@gmail.com>
2018-05-12 22:25:34 +0200

    1.2.1

--- Tests/PriceTest.php
+++ Tests/PriceTest.php
@@ -8,8 +8,7 @@ namespace JDZ\Price;
 use PHPUnit\Framework\TestCase;
 
 /**
- * @package Test
- * @author  Joffrey Demetz <joffrey.demetz@gmail.com>
+ * @author Joffrey Demetz <joffrey.demetz@gmail.com>
  */
 class PriceTest extends TestCase
 {
